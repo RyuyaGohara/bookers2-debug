268f62a16495193f9f905e775ca894b56fceb414
RyuyaGohara <138365197+RyuyaGohara@users.noreply.github.com>
2023-12-14 00:32:24 +0000

    users_controller コメント追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,7 @@
 class RoomsController < ApplicationController
+  
+  users show,controllerの読み解きから始める
+  
+  コード追加はこれから始める
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,25 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 #DM機能のため追加 ---------------------------
+  #whereメソッドとは、テーブル内の条件に一致したレコードを取得することができるメソッド
+  #roomがcreateされた時に現在ログインしているユーザーと、「チャットへ」ボタンを押されたユーザーの両方をEntriesテーブルに記録する
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
+
+  #現在ログインしているユーザーではないという条件
+  #すでにroomが作成されている場合とされていない場合に分岐
     if @user.id == current_user.id
+    #作成されている場合、 @currentUserEntryと@userEntryをeachで一つずつ取り出し、それぞれEntriesテーブル内にあるroom_idが共通しているユーザー同士に対して@roomId = cu.room_idという変数を指定
     else
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
           if cu.room_id == u.room_id then
-            @isRoom = true
+            @isRoom = true #@isRoom = trueは、falseのとき（Roomを作成するとき）の条件を分岐するための記述
             @roomId = cu.room_id
           end
         end
       end
+      #if @isRoom内では、新しくインスタンスを生成するために.newを記述
       if @isRoom
       else
         @room = Room.new
